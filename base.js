1fdf2273b1de07d178208c4c5bc61494b6a5cc8f
brendanbohn <brendanbohn@comcast.net>
2015-10-12 19:05:12 -0700

    slight color and name changes

--- base.js
+++ base.js
@@ -13,22 +13,26 @@ $(document).ready(function(){
 
 /*   detects if creat a post is empty  */
   	if ($('#create-post').val().trim().length > 0) {
+
+      /*  keeps track of how many posts there are   */
+      numPosts++;
+      // console.log(numPosts);
+
   		var postInput = $('#create-post').val();
       // console.log(postInput);
-      var newPost = '<p class="list-group-item">' + postInput + '</p>';
+      var newPost = '<p class="list-group-item">' + postInput + ' <span class="pull-right" id="post-number">Post #'+ numPosts+'</p>';
       $('#create-post').val('');
       $('#post-list').append(newPost);
       
-/* 	keeps track of how many posts there are 	*/
 
-      numPosts++;
-      // console.log(numPosts);
 
 /* 	creates a button to clear posts after the first post is created 	*/
 
       if (numPosts===1) {
       	$('#post-list').after('<button data-toggle="modal" data-target=".clear-modal-md" class="btn btn-default pull-right" id="clear-posts">Clear Posts</button>');
       }
+      $('#post-count').empty();
+      $('#post-list').after('<p id="post-count">Total posts: ' + numPosts+ '</p>');
 
   	}
 
@@ -44,6 +48,7 @@ $(document).ready(function(){
 
     $('#modal-clear-button').click(function(e){
       $('#post-list').empty();
+      $('#post-count').empty();
       $('#clear-posts').hide();
       numPosts = 0;
       $('#model-clear').modal("hide");
